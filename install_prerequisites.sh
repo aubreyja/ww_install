f4e82a7aa44bc1973211a06018b078b0f4bb7fc1
Nathaniel Case <Qalthos@gmail.com>
2012-06-04 15:31:23 -0400

    I guess we need subversion after all

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -2,7 +2,7 @@
 
 if [ -e "/etc/redhat-release" ]
 then
-	sudo yum install dvipng gcc libapreq2 mod_perl mysql-server perl-DateTime perl-Email-Address perl-GD perl-GDGraph perl-LDAP perl-libapreq2 perl-Locale-Maketext-Lexicon perl-Mail-Sender perl-PHP-Serialization perl-PadWalker perl-SOAP-Lite perl-SQL-Abstract perl-String-ShellQuote perl-Tie-IxHash system-config-services tex-preview uuid-perl perl-IPC-Cmd perl-Term-UI git
+	sudo yum install dvipng gcc libapreq2 mod_perl mysql-server perl-DateTime perl-Email-Address perl-GD perl-GDGraph perl-LDAP perl-libapreq2 perl-Locale-Maketext-Lexicon perl-Mail-Sender perl-PHP-Serialization perl-PadWalker perl-SOAP-Lite perl-SQL-Abstract perl-String-ShellQuote perl-Tie-IxHash system-config-services tex-preview uuid-perl perl-IPC-Cmd perl-Term-UI git subversion
 else
     echo "I don't know what packages you need.  Fork me to fix this!"
 fi
