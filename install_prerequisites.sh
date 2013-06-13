b3d5250fc19ef73cc1cccd443bd87add009e5b8b
Jason Aubrey <aubreyja@gmail.com>
2013-06-13 13:12:22 -0500

    Added git to list of ubuntu prerequisties

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -8,7 +8,7 @@ then
     sudo /usr/bin/mysql_secure_installation
 elif [ -e "/etc/debian_version" ]
 then
-    sudo apt-get -y install apache2 apache2-mpm-prefork apache2.2-common libapreq2 dvipng gcc perl perl-modules libapache2-request-perl libdatetime-perl libdbi-perl libdbd-mysql-perl libemail-address-perl libexception-class-perl libextutils-xsbuilder-perl libgd-gd2-perl liblocale-maketext-lexicon-perl libmail-sender-perl libmime-perl libnet-ip-perl libnet-ldap-perl libnet-oauth-perl libossp-uuid-perl libpadwalker-perl libphp-serialization-perl libsoap-lite-perl libsql-abstract-perl libstring-shellquote-perl libtimedate-perl libuuid-tiny-perl libxml-parser-perl libxml-writer-perl libpod-wsdl-perl libjson-perl libtext-csv-perl libhtml-scrubber-perl make mysql-server netpbm openssh-server preview-latex-style subversion texlive unzip
+    sudo apt-get -y install apache2 apache2-mpm-prefork apache2.2-common libapreq2 dvipng gcc perl perl-modules libapache2-request-perl git libdatetime-perl libdbi-perl libdbd-mysql-perl libemail-address-perl libexception-class-perl libextutils-xsbuilder-perl libgd-gd2-perl liblocale-maketext-lexicon-perl libmail-sender-perl libmime-perl libnet-ip-perl libnet-ldap-perl libnet-oauth-perl libossp-uuid-perl libpadwalker-perl libphp-serialization-perl libsoap-lite-perl libsql-abstract-perl libstring-shellquote-perl libtimedate-perl libuuid-tiny-perl libxml-parser-perl libxml-writer-perl libpod-wsdl-perl libjson-perl libtext-csv-perl libhtml-scrubber-perl make mysql-server netpbm openssh-server preview-latex-style subversion texlive unzip
     sudo cpan -j cpan_config.pm XML::Parser::EasyTree HTML::Template Iterator Iterator::Util
     sudo a2enmod apreq
     sudo apache2ctl restart
