1dfdaa801892d9aae588c5d6672493bba611a248
Jason Aubrey <aubreyja@gmail.com>
2013-06-15 13:48:04 -0500

    Corrected two mistakes in write_launch_browser_script()

--- ww_install.pl
+++ ww_install.pl
@@ -1824,8 +1824,14 @@ sub write_launch_browser_script {
 
   #We want to open the browser as the user that logged in, not root.
   my $username = $ENV{SUDO_USER};
-  my $uid = getpwnam($username);
-  my $gid = getgrnam($username);
+
+  #Remember that we used User::pwent which overrides the builtin pw* functions.
+  my $pw = getpwnam($username);
+  my $uid = $pw->uid;
+  my $gid = $pw->gid;
+
+  #Now we need to get back to starting dir
+  chdir $dir;
 
   #Get preferred web brwoser
 	my $browser = can_run('xdg-open') || can_run('x-www-browser') || can_run('www-browser') || can_run('gnome-open') || can_run('firefox'); 
