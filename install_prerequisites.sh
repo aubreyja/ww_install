e2c8039f4bf5daf6c748fe3a2f601e4fd6f296f4
Jason Aubrey <aubreyja@gmail.com>
2013-07-03 17:06:10 -0500

    Answer yes to yum

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -16,8 +16,8 @@ EOM
 }
 
 yum_install () {
-    sudo yum update
-    sudo yum install dvipng gcc libapreq2 mod_perl mysql-server perl-CPAN perl-DateTime perl-Email-Address perl-GD perl-GDGraph perl-LDAP perl-libapreq2 perl-Locale-Maketext-Lexicon perl-Mail-Sender perl-PHP-Serialization perl-PadWalker perl-SOAP-Lite perl-SQL-Abstract perl-String-ShellQuote perl-Tie-IxHash system-config-services tex-preview uuid-perl perl-IPC-Cmd perl-Term-UI git subversion perl-Exception-Class perl-Net-IP perl-XML-Parser
+    sudo yum -y update
+    sudo yum -y install dvipng gcc libapreq2 mod_perl mysql-server perl-CPAN perl-DateTime perl-Email-Address perl-GD perl-GDGraph perl-LDAP perl-libapreq2 perl-Locale-Maketext-Lexicon perl-Mail-Sender perl-PHP-Serialization perl-PadWalker perl-SOAP-Lite perl-SQL-Abstract perl-String-ShellQuote perl-Tie-IxHash system-config-services tex-preview uuid-perl perl-IPC-Cmd perl-Term-UI git subversion perl-Exception-Class perl-Net-IP perl-XML-Parser
 }
 
 if [ -e "/etc/redhat-release" ]
