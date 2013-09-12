f01065eef3af9b4e6504087b4fd8043522a971be
Jason Aubrey <aubreyja@gmail.com>
2013-09-12 15:21:15 -0700

    Added sub edit_httpd_conf to address issue #32

--- ww_install.pl
+++ ww_install.pl
@@ -1998,6 +1998,76 @@ sub write_webwork_apache2_config {
     }
 }
 
+sub edit_httpd_conf {
+  my $apache = shift;
+  my $httpd_conf = $apache->{conf};
+
+  my (undef,$dir,$file) = File::Spec->splitpath($httpd_conf);
+
+  #Make a backup copy of the apache config file
+  copy($httpd_conf,$dir."/".$file.".bak");
+
+  my $print_me = <<END;
+#######################################################################
+#
+# Next, I would like to increase apache's page timout value from 300
+# to 1200. 
+#
+#######################################################################
+END
+  my $prompt = "Please enter a value for Timeout:";
+  my $default = 1200;
+  my $timeout = get_reply($print_me,$prompt,[],$default);
+
+   
+  $print_me = <<END;
+###################################################################
+#
+# Now I would like to modify the prefork MPM
+# settings MaxClients and MaxRequestsPerChild. By default I'll change 
+# MaxClients from 150 to 20 and MaxRequestsPerChild from 0 to 100.
+#
+# For WeBWorK a rough rule of thumb is 20 MaxClients per 1 GB of 
+# memory.  So, e.g., if you have 4GB of RAM you may want to use
+# MaxClients 80.
+# 
+######################################################################
+END
+  $prompt = "Please enter a value for prefork MaxClients:";
+  $default = 20;
+  my $max_clients = get_reply($print_me,$prompt,[],$default);
+
+  $prompt = "Please enter a value for prefork MaxRequestsPerChild:";
+  $default = 100;
+  my $max_requests_per_child = get_reply('',$prompt,[],$default);
+
+
+  #Open apache config file for reading 
+  open(my $fh, '<',$httpd_conf)
+    or die "Couldn't open $httpd_conf for reading: $!\n";
+  #read it into a string
+  my $string = do { local($/); <$fh> };
+  close($fh);
+
+  #Make replacements
+  if($string =~ /(Timeout\s+\d+)/s) {
+   print "$1 \n";
+   $string =~ s/$1/Timeout $timeout/;
+  }
+  if($string =~ /\<IfModule mpm\_prefork\_module\>.*?(MaxClients\s+\d+).*?\<\/IfModule\>/s) {
+    $string =~ s/$1/MaxClients           $max_clients/;
+  }
+  if($string =~ /\<IfModule mpm\_prefork\_module\>.*?(MaxRequestsPerChild\s*\d+).*?\<\/IfModule\>/s) {
+    $string =~ s/$1/MaxRequestsPerChild $max_requests_per_child/;
+  }
+
+  #Open apache config file for writing and write!
+  open($fh, '>',$httpd_conf)
+    or die "Couldn't open $httpd_conf for writing: $!\n";
+  print $fh $string;
+  close($fh);
+}
+
 ##########################################################
 #
 #  Configure environment (symlink webwork-apache2.config,
@@ -2292,16 +2362,23 @@ write_webwork_apache2_config("$webwork_dir");
 print_and_log(<<EOF);
 #######################################################################
 #
-# Well, that was easy.  Now i'm going to symlink webwork.apache2-config
-# to your apache conf.d dir as webwork.conf. 
+# Well, that was easy.  Now I need to slightly modify the 
+# configuration of your apache webserver.  
 #
-# 
-######################################################################
+# First, I'm going symlink webwork.apache2-config to your apache 
+# conf.d dir as webwork.conf. This will have the effect of starting 
+# webwork whenever the webserver is started.  This *must* be done
+# to enable webwork, so sorry - no choice in this matter.
+#
+#######################################################################
 EOF
+
 symlink(
     "$webwork_dir/conf/webwork.apache2-config",
     $apache->{root} . "/conf.d/webwork.conf"
 );
+
+edit_httpd_conf($apache);
 
 print_and_log(<<EOF);
 #######################################################################
