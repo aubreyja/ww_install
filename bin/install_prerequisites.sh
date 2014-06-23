281ad317d10305d2f9bbdd2e222860b4da19ffd5
Geoff Goehle <goehle@gmail.com>
2014-06-23 09:29:29 -0400

    Update install_prerequisites.sh

--- bin/install_prerequisites.sh
+++ bin/install_prerequisites.sh
@@ -80,11 +80,11 @@ then
   fi
    yum -y update
   yum_install
-   cpan $CPANOPT -f Test::XML
+   cpan $CPANOPT -f Test::XML Pod::WSDL # for some reason these don't install without force in fedora/centos
    cpan $CPANOPT XML::Parser::EasyTree Iterator Iterator::Util UUID::Tiny HTML::Template PHP::Serialization Env
    #ww3
    cpan $CPANOPT Dancer Dancer::Plugin::Database Plack::Runner Plack::Handler::FCGI Path::Class Array::Utils YAML Template
-   cpan $CPANOPT File::Find::Rule Path::Class FCGI File::Slurp Pod::WSDL
+   cpan $CPANOPT File::Find::Rule Path::Class FCGI File::Slurp
   $MYSQLSTART
   $MYSQLENABLE
   $APACHESTART
