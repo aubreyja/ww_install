b41deb7167007070cf2231b93e77634a4c62e6cb
Geoff Goehle <goehle@gmail.com>
2014-07-25 11:32:46 -0400

    Update install_prerequisites.sh

--- bin/install_prerequisites.sh
+++ bin/install_prerequisites.sh
@@ -80,9 +80,9 @@ then
   fi
    yum -y update
   yum_install
-   # currently needed bcause cpan doesnt find these prerequsities for Pod::WSDL
+  # currently needed bcause cpan doesnt find these prerequsities for Pod::WSDL and Test::XML is broken
    cpan $CPANOPT Module::Build Fatal XML::SAX
-   cpan $CPANOPT Test::XML    
+   cpan $CPANOPT -f Test::XML    
    cpan $CPANOPT XML::Parser::EasyTree Iterator Iterator::Util UUID::Tiny HTML::Template PHP::Serialization Env Pod::WSDL
    #ww3
    cpan $CPANOPT Dancer Dancer::Plugin::Database Plack::Runner Plack::Handler::FCGI Path::Class Array::Utils Template
